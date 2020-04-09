9c34f59a98578d8d7f3b920aecf01ee27f9b8f60
itaym <itaymer@gmail.com>
2020-04-09 21:11:14 +0300

    Adding usePlaceholder, Editing readme.md

--- README.md
+++ README.md
@@ -1,10 +1,33 @@
 # isEllipsis
-Find out if an HTML element has ellipsis.
+Find out if an HTML tag or an Input tag currently display ellipsis.
 
-# Usage
-import { isEllipsis } from "isellipsis";
+## Usage
+- import { isEllipsis } from "isellipsis";
+- let value = isEllipsis(HTMLElement, usePlaceholder);
+##### Variables
+- value : Zero or greater indicating an ellipsis is shown.
+  If an error occurred isEllipsis returns 0.
+- HTMLElement : Any HTML element such as DIV or Input etc.
+- usePlaceholder : Indicates whenever to use the placeholder (default is true). 
 
-let overflowWithEllipsis = isEllipsis(document.getElementById('element-id'))
+#### Overall
+The isEllipsis returns a greater from zero number only if the HTMLElement is set up for ellipsis and the content overflows the element content area.
+So, if the returned value is greater from zero the HTMLElement displays ellipsis (...).
 
-console.log(\`Is Ellipsis: ${!!overflowWithEllipsis}\`;
-console.log(\`Overflow by: ${overflowWithEllipsis} pixels\`;
+#### Example
+```
+<style>
+    INPUT {
+        white-space: nowrap;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        width: 200px;
+    }
+</style>
+<input id="some-info" type="text" placeholder="This is a long placeholder with ellipsis" />
+
+let someInfo = document.getElementById("some-info");
+if (isEllipsis(someInfo)) {
+    console.log("ellipsis is visible");
+}
+ ```
--- src/index.ts
+++ src/index.ts
@@ -2,21 +2,20 @@ const copyComputedStyles = (source: HTMLElement, target: HTMLElement): CSSStyleD
   const computedStyles: CSSStyleDeclaration = window.getComputedStyle(source);
 
   for (const computedStyle of Array.from(computedStyles)) {
-    // @ts-ignore
-    target.style[computedStyle] = computedStyles.getPropertyValue(computedStyle);
+    target.style[computedStyle as any] = computedStyles.getPropertyValue(computedStyle);
   }
   return computedStyles;
 };
 
-const getInputValue = (inputElement: HTMLInputElement): string => {
-  return inputElement.value || inputElement.placeholder;
+const getInputValue = (inputElement: HTMLInputElement, usePlaceholder: boolean): string => {
+  return inputElement.value || (usePlaceholder ? inputElement.placeholder : '');
 };
 
-const getValue = (element: HTMLElement): string => {
-  return element.tagName === 'INPUT' ? getInputValue(element as HTMLInputElement) : element.innerHTML;
+const getValue = (element: HTMLElement, usePlaceholder:boolean): string => {
+  return element.tagName === 'INPUT' ? getInputValue(element as HTMLInputElement, usePlaceholder) : element.innerHTML;
 };
 
-export const isEllipsis = (sourceElement: HTMLElement): number => {
+export const isEllipsis = (sourceElement: HTMLElement, usePlaceholder:boolean = true): number => {
   const copiedElement: HTMLDivElement = document.createElement('DIV') as HTMLDivElement;
   const holderElement: HTMLSpanElement = document.createElement('SPAN') as HTMLSpanElement;
 
@@ -24,7 +23,7 @@ export const isEllipsis = (sourceElement: HTMLElement): number => {
   document.body.appendChild(copiedElement);
 
   const computedStyles: CSSStyleDeclaration = copyComputedStyles(sourceElement, copiedElement);
-  const innerHtml: string = getValue(sourceElement);
+  const innerHtml: string = getValue(sourceElement, usePlaceholder);
 
   copiedElement.style.width = `${sourceElement.offsetWidth}px`;
   copiedElement.style.height = `${sourceElement.offsetHeight}px`;
